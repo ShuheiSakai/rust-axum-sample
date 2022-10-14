75000795b852e4296ac48d10cee3b987da1dae6b
ShuheiSakai <darshuheider@gmail.com>
2022-10-14 09:37:44 +0900

    add axum Hello World sample

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new().route("/",
+        get(|| async { "Hello, World" }));
+    
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
