4e0bd4fa26eab4d300cb799d96cc8ed295acc292
ShuheiSakai <darshuheider@gmail.com>
2022-10-15 10:59:15 +0900

    add hyper sample

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use axum::routing::get;
+use std::convert::Infallible;
+use hyper::Body;
+
+async fn handle(_: hyper::Request<Body>) -> Result<hyper::Response<hyper::Body>, Infallible> {
+    Ok(hyper::Response::new("Hello, World!".into()))
+}
 
 #[tokio::main]
 async fn main() {
-    let app = axum::Router::new().route("/",
-        get(|| async { "Hello, World" }));
-    
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let make_svc = hyper::service::make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(hyper::service::service_fn(handle))
+    });
+
+    let server = hyper::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
